2fc01c11fa90db08471b935371dd39cd3c7d6cd4
Otakar Vydra <vydraotakar@gmail.com>
2021-01-24 21:58:54 +0100

    File created

---
+++ multi_effect_evaporation.py
@@ -0,0 +1,43 @@
+   #Imports
+   from gas_cp_calculation import gas_cp_data
+   from gas_cp_calculation import gas_sensible_heat as cp
+   from compound_properties import H20 as water
+   
+    #Define the initial conditions of the system 
+
+W_f = 5   #kg/hr
+C_f = 5   #wt%
+T_f = 25  #deg C
+
+W_S = 10  #kg/hr
+T_S = 200 #deg C
+P_S = 2.5 #bara
+
+    #Define the pressure of each evaporator
+
+P_all = [1, 0.8, 0.6, 0.4, 0.2] #bara
+
+    #Define empty lists that will then be used to store important parameters 
+
+T_all     = []
+C_f_all   = []
+W_v_all   = []
+
+    #Engine 
+
+for i in P_all:
+    
+    Q_sens 
+    Q_latent
+    W_v =     
+     
+    #Define new variables
+    W_f =
+    C_f = 
+    T_f = 
+    W_s = 
+    T_s = 
+    P_s = 
+
+
+    #Display results
